cf2a5ff51c8b4bb1ace5d6dd1b17307f4a31605a
chaks-carnot <chakshu.gautam@carnot.co.in>
2017-01-20 14:38:01 +0530

    Config added

--- includes/config.php
+++ includes/config.php
@@ -1,33 +1,34 @@
-<?php 
-	//----------------------------------------------------------------------------------//	
+<?php
+	//----------------------------------------------------------------------------------//
 	//                               COMPULSORY SETTINGS
 	//----------------------------------------------------------------------------------//
-	
+
 	/*  Set the URL to your Sendy installation (without the trailing slash) */
-	define('APP_PATH', 'http://your_sendy_installation_url');
-	
+	define('APP_PATH', 'https://carnotmail.herokuapp.com/');
+	// mysql://b9809b7f27ca86:6aedbe38@us-cdbr-iron-east-04.cleardb.net/heroku_d9311a2a20ecb21?reconnect=true
+
 	/*  MySQL database connection credentials (please place values between the apostrophes) */
-	$dbHost = ''; //MySQL Hostname
-	$dbUser = ''; //MySQL Username
-	$dbPass = ''; //MySQL Password
-	$dbName = ''; //MySQL Database Name
-	
-	
-	//----------------------------------------------------------------------------------//	
+	$dbHost = 'us-cdbr-iron-east-04.cleardb.net'; //MySQL Hostname
+	$dbUser = 'b9809b7f27ca86'; //MySQL Username
+	$dbPass = '6aedbe38'; //MySQL Password
+	$dbName = 'heroku_d9311a2a20ecb21'; //MySQL Database Name
+
+
+	//----------------------------------------------------------------------------------//
 	//								  OPTIONAL SETTINGS
-	//----------------------------------------------------------------------------------//	
-	
-	/* 
+	//----------------------------------------------------------------------------------//
+
+	/*
 		Change the database character set to something that supports the language you'll
 		be using. Example, set this to utf16 if you use Chinese or Vietnamese characters
 	*/
 	$charset = 'utf8';
-	
+
 	/*  Set this if you use a non standard MySQL port.  */
-	$dbPort = 3306;	
-	
+	$dbPort = 3306;
+
 	/*  Domain of cookie (99.99% chance you don't need to edit this at all)  */
 	define('COOKIE_DOMAIN', '');
-	
+
 	//----------------------------------------------------------------------------------//
 ?>
