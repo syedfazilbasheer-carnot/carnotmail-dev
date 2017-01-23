69c3a013ad4886c9c027083c11edf7140a731c58
chaks-carnot <chakshu.gautam@carnot.co.in>
2017-01-23 15:48:30 +0530

    added 2 buttons

--- js/create/main.php
+++ js/create/main.php
@@ -1,10 +1,10 @@
 <script type="text/javascript">
 $(document).ready(function() {
-		
+
 	//send test email
 	$("#test-form").submit(function(e){
-		e.preventDefault(); 
-		
+		e.preventDefault();
+
 		var $form = $(this),
 		test_email = $form.find('input[name="test_email"]').val(),
 		campaign_id = $form.find('input[name="cid"]').val(),
@@ -13,22 +13,22 @@ $(document).ready(function() {
 		//validate email
 		AtPos = test_email.indexOf("@")
 		StopPos = test_email.lastIndexOf(".")
-		
+
 		if(test_email=='')
 		{
 			$("#test-send-btn").effect("shake", { times:3 }, 60);
 		}
-		else if (AtPos == -1 || StopPos == -1) 
+		else if (AtPos == -1 || StopPos == -1)
 		{
 			$("#test-send-btn").effect("shake", { times:3 }, 60);
 		}
 		else
 		{
 			$("#test-send-btn").html("<i class=\"icon icon-envelope-alt\"></i> <?php echo _('Sending');?>...");
-			
+
 			$.post(url, { test_email: test_email, campaign_id: campaign_id, webversion: webversion },
 			  function(data) {
-			  
+
 			  	  $("#test-send").css("display", "none");
 			  	  $("#test-send-error").css("display", "none");
 			  	  $("#test-send-btn").html("<i class=\"icon icon-envelope-alt\"></i> <?php echo _('Test send this newsletter');?>");
@@ -37,7 +37,7 @@ $(document).ready(function() {
 			);
 		}
 	});
-	
+
 	//select list count
 	$("select#email_list").change(function () {
       var recip = 0;
@@ -45,7 +45,7 @@ $(document).ready(function() {
             recip += Number($(this).data('quantity'));
           });
       $("#recipients").text(recip);
-      
+
       //if user have AWS keys, check quota
       if($("#aws_keys_available").val()=='true' || $("#is_sub_user").val())
 	  {
@@ -57,8 +57,8 @@ $(document).ready(function() {
 		      $("#recipients").css("font-weight", "bold");
 		      $("#real-btn").addClass("disabled");
 		      $("#real-btn").attr("disabled", "disabled");
-		      $("#schedule-btn").addClass("disabled");
-		      $("#schedule-btn").attr("disabled", "disabled");
+		    //   $("#schedule-btn").addClass("disabled");
+		    //   $("#schedule-btn").attr("disabled", "disabled");
 		      $("#pay-btn").addClass("disabled");
 		      $("#pay-btn").attr("disabled", "disabled");
 	      }
@@ -75,22 +75,22 @@ $(document).ready(function() {
 		      $("#pay-btn").removeAttr("disabled");
 	      }
 	   }
-	   
+
 	   $("#grand_total").text(
       	number_format(
-	      	Number($("#delivery_fee").text()) + 
+	      	Number($("#delivery_fee").text()) +
 	      	(Number($("#recipient_fee").text()) * recip)
       	, 3, '.', ',')
       );
       $("#grand_total_val").val(
       	number_format(
-	      	Number($("#delivery_fee").text()) + 
+	      	Number($("#delivery_fee").text()) +
 	      	(Number($("#recipient_fee").text()) * recip)
       	, 2, '.', ',')
       );
     })
     .trigger('change');
-	
+
 	//number format function
 	function number_format( number, decimals, dec_point, thousands_sep ) {
 	    var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
@@ -99,7 +99,7 @@ $(document).ready(function() {
 	    var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
 	    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 	}
-	
+
 	//send later
 	$("#send-later-btn").click(function(){
 		if($("#schedule-form-wrapper").is(":visible"))
@@ -117,12 +117,12 @@ $(document).ready(function() {
 			$("#send-later-btn").html("&larr; <?php echo _('Back');?>");
 		}
 	});
-	
+
 	$('#datepicker').datepicker({
 		format: 'mm-dd-yyyy',
 		weekStart: 1
 	});
-	
+
 	$("#schedule-form").submit(function(){
 		$("#email_lists").val($('select#email_list').val());
 		$("#grand_total_val2").val($('#grand_total_val').val());
